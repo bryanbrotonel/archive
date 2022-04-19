8c5364f9d56b86466e83bdf01f3d4bafd298f5e2
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-04-18 17:58:52 -0700

    Implemented Spotify artist API

---
+++ src/api/SpotifyArtistAPI.js
@@ -0,0 +1,29 @@
+import getSpotifyAuth from './SpotifyAuthAPI';
+
+const axios = require('axios').default;
+
+// Fetch Spotify artist info
+export async function getArtist(id, param = '', query = null) {
+  // Get Spotify auth token
+  const auth = await getSpotifyAuth();
+
+  // Token url for API request
+  const token_url = `https://api.spotify.com/v1/artists/${id}/${param}`;
+
+  // API headers
+  const headers = {
+    Authorization: `Bearer ${auth}`,
+  };
+
+  try {
+    // Fetch data using Axios
+    const response = await axios.get(token_url, {
+      headers: headers,
+      params: query,
+    });
+
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
--- src/api/SpotifyAuthAPI.js
+++ src/api/SpotifyAuthAPI.js
@@ -2,9 +2,12 @@ const axios = require('axios').default;
 const Buffer = require('buffer/').Buffer;
 const qs = require('qs');
 
+// Fetch Spotify authentication access token
 async function getSpotifyAuth() {
+  // Token url for API request
   const token_url = 'https://accounts.spotify.com/api/token';
 
+  // API headers
   const headers = {
     Authorization: `Basic ${Buffer.from(
       `${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`,
@@ -13,12 +16,16 @@ async function getSpotifyAuth() {
     'Content-Type': 'application/x-www-form-urlencoded',
   };
 
+  // Format grant-type to string
   const form = qs.stringify({ grant_type: 'client_credentials' });
 
   try {
+    // Fetch data using Axios
     const response = await axios.post(token_url, form, {
       headers: headers,
     });
+
+    // Retrun auth access token
     return response.data.access_token;
   } catch (e) {
     console.log(e);
