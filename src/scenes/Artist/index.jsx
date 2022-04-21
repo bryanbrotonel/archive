23053a47586ef4b152af16aa7ada42e3a4f4789b
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-04-21 09:48:43 -0700

    Added artist page back end

--- src/scenes/Artist/index.jsx
+++ src/scenes/Artist/index.jsx
@@ -9,7 +9,11 @@ function Artist() {
 
   let params = useParams();
   let dataElement;
+
+  // Get artist parameter from url
   const linkParam = params.artistID;
+
+  // Capitalize artist parameter
   var capitalLinkParam =
     linkParam[0].toUpperCase() + linkParam.slice(1).toLowerCase();
 
@@ -24,39 +28,73 @@ function Artist() {
   }`;
 
   useEffect(() => {
+    // Fetch artist data from Contentful
     getContentfulData(contentfulQuery, 'artistCollection').then(
       (contentfulRes) => {
-        getSpotifyArtist(contentfulRes.artistId).then((spotifyRes) => {
-          console.log(contentfulRes);
-          console.log(spotifyRes);
-          setArtist({
-            name: spotifyRes.name,
-            bio: contentfulRes.artistBio,
-            image: spotifyRes.images[0],
-            genres: spotifyRes.genres,
-          });
+        if (contentfulRes.length == 0) {
           setIsLoaded(true);
-        });
+        } else {
+          // Fetch artist data from Spotify
+          const artistData = getSpotifyArtist(contentfulRes.artistId);
+
+          // Fetch artist top tracks from Spotify
+          const topTracks = getSpotifyArtist(
+            contentfulRes.artistId,
+            'top-tracks',
+            { market: 'ES' }
+          );
+
+          Promise.all([artistData, topTracks]).then(function (values) {
+            const spotifyArtistData = values[0];
+            const spotifyTopTracks = values[1];
+
+            // Create artist object with artist data
+            setArtist({
+              name: spotifyArtistData.name,
+              bio: contentfulRes.artistBio,
+              image: spotifyArtistData.images[0],
+              genres: spotifyArtistData.genres,
+              tracks: spotifyTopTracks.tracks,
+              url: spotifyArtistData.external_urls.spotify,
+            });
+            setIsLoaded(true);
+          });
+        }
       }
     );
   }, []);
 
   if (isLoaded) {
-    const { name, bio, image, genres } = artist;
+    if (artist == null)
+      dataElement = (
+        <div>
+          <h1>Artist not Found</h1>
+          <p>Is that a new artist? Share it with the world!</p>
+        </div>
+      );
+    else {
+      const { name, bio, image, genres, tracks, url } = artist;
 
-    dataElement = (
-      <div>
-        <img
-          src={image.url}
-          alt={`${name} - Image`}
-          width={`${image.width}`}
-          height={`${image.height}`}
-        />
-        <h1>{name}</h1>
-        <h4>{genres.join(' ')}</h4>
-        <p>{bio}</p>
-      </div>
-    );
+      dataElement = (
+        <div>
+          <img
+            src={image.url}
+            alt={`${name} - Image`}
+            width={`${image.width}`}
+            height={`${image.height}`}
+          />
+          <h1>{name}</h1>
+          <h4>{genres.join(' ')}</h4>
+          <p>{bio}</p>
+          <ul>
+            {tracks.map((track) => (
+              <li key={track.id}>{track.name}</li>
+            ))}
+          </ul>
+          <a href={url}>Listen to more {name} on Spotify</a>
+        </div>
+      );
+    }
   }
 
   return (
