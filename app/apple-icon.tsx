7b6f21282e842d33c6377671bed6f8ad5c68641d
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2025-06-20 15:55:14 -0700

    Updaed favicons

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -1,5 +1,5 @@
+import Image from 'next/image';
 import { ImageResponse } from 'next/og';
-import AppIcon from './ui/AppIcon';
 
 export const size = {
   width: 32,
@@ -8,5 +8,28 @@ export const size = {
 export const contentType = 'image/svg+xml';
 
 export default function Icon() {
-  return new ImageResponse(<AppIcon size={32} />, size);
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#fbfbf2',
+          padding: 3,
+        }}
+      >
+        <Image
+          src='/images/logo.svg'
+          alt="Bryan's Archive Logo"
+          width={100}
+          height={100}
+          priority
+        />
+      </div>
+    ),
+    size
+  );
 }
