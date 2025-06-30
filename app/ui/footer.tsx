0b9410fc89381f7745da817b42929f590ee82dc5
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2025-06-30 12:33:37 -0700

    Updated footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -4,13 +4,14 @@ export default function Footer() {
   return (
     <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center font-[family-name:var(--font-roboto-mono)]'>
       <span className='text-xs'>
+        Made with&nbsp;&#10084;&#65039;&nbsp;by&nbsp;
         <a
           href='https://bryanbrotonel.vercel.app/'
           target='_blank'
           rel='noopener noreferrer'
           className='hover:underline'
         >
-          Bryan Brotonel
+          Bryan
         </a>
       </span>
     </footer>
